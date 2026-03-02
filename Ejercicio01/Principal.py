5674a25852447cceb649902735cdc7bf59bcf433
chambiquispehelen <chambiquispehelen@gmail.com>
2026-03-02 14:00:34 -0400

    cambios en principal

--- Ejercicio01/Principal.py
+++ Ejercicio01/Principal.py
@@ -2,19 +2,18 @@ from anime import anime
 from televisor import Televisor
 from instrumento import Instrumento
 
-    # Crear objetos Anime
 a1 = anime("Naruto", "Shonen", 220)
 a2 = anime("Death Note", "Misterio", 102)
-
-    # Crear objetos Televisor
+    
 t1 = Televisor("Chic", 4, "OLED")
 t2 = Televisor("Sony", 8, "IPS")
 
-    # Crear objetos Instrumento
 i1 = Instrumento("Violin", "Madera", "Cuerda")
 i2 = Instrumento("Flauta", "Metal", "Aire")
 
-    # Mostrar datos
+print("Anime1: ", a1)
+print("Anime2: ", a2)
+
 print("Televisor:", t1)
 print("Televisor2:", t2)
 
